3b5cfc5aec4ca05158aca600c96f88160b702bed
fumiko <fumi811bbc@gmail.com>
2014-12-08 23:42:38 +0900

    first commit

---
+++ fantasticPics/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  fantasticPics
+//
+//  Created by 山本文子 on 2014/12/08.
+//  Copyright (c) 2014年 山本文子. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ fantasticPics/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  fantasticPics
+//
+//  Created by 山本文子 on 2014/12/08.
+//  Copyright (c) 2014年 山本文子. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
